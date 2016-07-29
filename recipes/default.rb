d6189f2b35c0b1e8f0049f201264b4ffc49e2810
Felipe Cecagno <fcecagno@gmail.com>
2016-07-29 00:08:13 -0300

    let certbot maintain the certificates under /etc/letsencrypt, but create symlinks to the path set by the attributes

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,15 +14,15 @@ remote_file node['certbot']['bin']['path'] do
   source node['certbot']['bin']['download_uri']
 end
 
-cmd = [ node['certbot']['bin']['path'], "certonly", "--non-interactive", "--register-unsafely-without-email", "--agree-tos" ]
+cmd = [ node['certbot']['bin']['path'], "certonly", "--non-interactive", "--register-unsafely-without-email", "--agree-tos", "--expand" ]
 cmd << "--standalone" if node['certbot']['standalone']
 cmd << "--webroot-path #{node['certbot']['webroot']}" if node['certbot']['webroot'].to_s != ''
+cmd << "--pre-hook \"#{node['certbot']['pre_hook']}\"" if node['certbot']['pre_hook'].to_s != ''
 cmd << "--post-hook \"#{node['certbot']['post_hook']}\"" if node['certbot']['post_hook'].to_s != ''
-cmd << "--cert-path #{node['certbot']['cert_path']}" if node['certbot']['cert_path'].to_s != ''
-cmd << "--chain-path #{node['certbot']['chain_path']}" if node['certbot']['chain_path'].to_s != ''
-cmd << "--fullchain-path #{node['certbot']['fullchain_path']}" if node['certbot']['fullchain_path'].to_s != ''
-cmd << "--key-path #{node['certbot']['key_path']}" if node['certbot']['key_path'].to_s != ''
-cmd << "--domains #{([] + node['certbot']['domains']).join(" ")}" if ! node['certbot']['domains'].empty?
+domains = [] + node['certbot']['domains']
+domains.each do |domain|
+  cmd << "--domain #{domain}"
+end
 Chef::Log.info "Executing: #{cmd.join(" ")}"
 
 execute "install certbot" do
@@ -33,12 +33,31 @@ end
 renew_cmd = [ node['certbot']['bin']['path'], "renew" ]
 renew_cmd << "--standalone" if node['certbot']['standalone']
 renew_cmd << "--webroot #{node['certbot']['webroot']}" if node['certbot']['webroot'].to_s != ''
+renew_cmd << "--pre-hook \"#{node['certbot']['pre_hook']}\"" if node['certbot']['pre_hook'].to_s != ''
 renew_cmd << "--post-hook \"#{node['certbot']['post_hook']}\"" if node['certbot']['post_hook'].to_s != ''
 renew_cmd << "--no-self-upgrade"
 renew_cmd << "--quiet"
 
-cron "auto renew" do
+cron "auto renew let's encrypt certificate" do
   hour [ Random.new.rand(0..11) ].map{ |n| [n, n+12] }.join(",")
   minute Random.new.rand(0..59).to_s
   command renew_cmd.join(" ")
+end
+
+links = {}
+links["cert.pem"] = node['certbot']['cert_path'] if node['certbot']['cert_path'].to_s != ''
+links["chain.pem"] = node['certbot']['chain_path'] if node['certbot']['chain_path'].to_s != ''
+links["fullchain.pem"] = node['certbot']['fullchain_path'] if node['certbot']['fullchain_path'].to_s != ''
+links["privkey.pem"] = node['certbot']['key_path'] if node['certbot']['key_path'].to_s != ''
+
+ruby_block "create symlinks" do
+  block do
+    links.each do |src, dst|
+      FileUtils.mkdir_p File.dirname(dst)
+      letsencrypt_certs = Dir.glob("/etc/letsencrypt/live/*").map{ |d| File.basename(d) }
+      intersection = domains & letsencrypt_certs
+      FileUtils.ln_s "/etc/letsencrypt/live/#{intersection.first}/#{src}", dst if ! intersection.empty?
+    end
+  end
+  action :run
 end
